b73550e95f2d5779940febf9c192886bec134709
chanbyeol <cksquf98@naver.com>
2024-09-23 00:04:44 +0900

    feat: 랜덤 questions 배열 리턴하는 api 추가

--- src/main/java/org/somemate/demo/question/dao/QuestionDao.java
+++ src/main/java/org/somemate/demo/question/dao/QuestionDao.java
@@ -4,8 +4,13 @@ import org.apache.ibatis.annotations.Param;
 import org.somemate.demo.question.dto.Question;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Mapper
 public interface QuestionDao {
     Question getQuestionById(@Param("questionId") int questionId) throws SQLException;
+    ArrayList<Question> getEIQuestions() throws SQLException;
+    Question getNSQuestion() throws SQLException;
+    ArrayList<Question> getFTQuestions() throws SQLException;
+    ArrayList<Question> getJPQuestions() throws SQLException;
 }
--- src/main/java/org/somemate/demo/question/service/QuestionServiceImp.java
+++ src/main/java/org/somemate/demo/question/service/QuestionServiceImp.java
@@ -5,6 +5,7 @@ import org.somemate.demo.question.dto.Question;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Service
 public class QuestionServiceImp implements QuestionService {
@@ -17,6 +18,26 @@ public class QuestionServiceImp implements QuestionService {
     @Override
     public Question getQuestionById(int questionId) throws SQLException {
         return questionDao.getQuestionById(questionId);
+    }
+
+    @Override
+    public ArrayList<Question> getEIQuestions() throws SQLException {
+        return questionDao.getEIQuestions();
+    }
+
+    @Override
+    public Question getNSQuestion() throws SQLException {
+        return questionDao.getNSQuestion();
+    }
+
+    @Override
+    public ArrayList<Question> getFTQuestions() throws SQLException {
+        return questionDao.getFTQuestions();
+    }
+
+    @Override
+    public ArrayList<Question> getJPQuestions() throws SQLException {
+        return questionDao.getJPQuestions();
     }
 
 }
